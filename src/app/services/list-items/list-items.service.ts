930e63958008382103c8657fa3a7e184d1eeaaac
Ahmed Abdelraheem <ahmed.abdelraheem225@gmail.com>
2018-09-06 01:22:03 +0200

    arrange and add category

---
+++ src/app/services/list-items/list-items.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListItemsService {
+  private items = [
+    { id: 1, name: 'Milk', category:'Dairy Products', status: false  },
+    { id: 2, name: 'Eggs', category:'Spready', status: false },
+    { id: 3, name: 'Potato', category:'Vegetables & Fruits', status: false },
+    { id: 4, name: 'Tomato', category:'Vegetables & Fruits', status: false },
+    { id: 5, name: 'Cheese', category:'Dairy Products', status: false },
+    { id: 6, name: 'Burger', category:'Meat', status: false },
+    { id: 7, name: 'Chicken', category:'Meat', status: false },
+    { id: 8, name: 'Fish', category:'Meat', status: false },
+    { id: 9, name: 'Juice', category:'Drinks', status: false }
+  ]
+  constructor() { }
+
+  getListItems(){
+    return of(this.items);
+  }
+}
